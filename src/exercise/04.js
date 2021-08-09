130be4b962dfda413ebd2df8c04fe66e58ec486a
Carlos Vieira <carlosadrianovieira@gmail.com>
2021-08-09 12:12:50 -0300

    exercise 04

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,20 +2,19 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
-import {Switch} from '../switch'
+import { Switch } from '../switch'
 
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  const togglerProps = { 'aria-pressed': on, onClick: toggle }
+
+  return { on, toggle, togglerProps }
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const { on, togglerProps } = useToggle()
   return (
     <div>
       <Switch on={on} {...togglerProps} />
